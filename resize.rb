a0b0c379a7031488bf40e6f04e574b5498848ac2
Kir Shatrov <shatrov@me.com>
2021-09-22 13:33:08 +0200

    More photos

--- resize.rb
+++ resize.rb
@@ -1,9 +1,14 @@
 Dir['assets/photography/**/*'].each do |path|
   next if File.directory?(path)
+  next if path.include?("_pix.jpg")
 
   dir = File.dirname(path)
   base = File.basename(path, ".*")
 
-  cmd = "convert -scale 10% -scale 500% #{path} #{dir}/#{base}_pix.jpg"
-  `#{cmd}`
+  pix_path = "#{dir}/#{base}_pix.jpg"
+
+  # unless File.exist?(pix_path)
+    cmd = "convert -scale 10% -scale 300% #{path} #{pix_path}"
+    `#{cmd}`
+  # end
 end
